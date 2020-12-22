183264a82d66133bc095ccc1bcad941878fc5d7c
wellington <wellingtonslpbi@hotmail.com>
2020-12-22 11:57:00 -0300

    add custom icons

---
+++ src/components/CustomIcon/index.tsx
@@ -0,0 +1,8 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import icoMoonConfig from '../../../selection.json';
+
+export default createIconSetFromIcoMoon(
+  icoMoonConfig,
+  'icomoon',
+  'icomoon.ttf',
+);
